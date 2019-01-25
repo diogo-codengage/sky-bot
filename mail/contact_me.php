3043e3ffd9aada9fc098016e3d3cf0e2d8a4b4f7
diogobiz <diogo@codengage.com>
2019-01-25 09:22:52 -0200

    Adde mailgun

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,13 @@
 <?php
+# Try running this locally.
+# Include the Autoloader (see "Libraries" for install instructions)
+require 'vendor/autoload.php';
+use Mailgun\Mailgun;
+
+# Instantiate the client.
+$mgClient = new Mailgun('93f2fb5379e64e3b81e1bf314d6c26e4-3939b93a-a5430d69');
+$domain = "sandbox395bf250df824cfb8c69a0479ac70f63.mailgun.org";
+
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
@@ -9,18 +18,18 @@ if(empty($_POST['name'])      ||
    echo "Nenhum argumento fornecido!";
    return false;
    }
+
+   echo "Nenhum argumento fornecido!";
    
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-   
-// Create the email and send the message
-$to = 'skybot@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Formulário de contato do site:  $name";
-$email_body = "Você recebeu uma nova mensagem do formulário de contato do seu site.\n\n"."Veja os detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nTelefonee: $phone\n\nMensagem:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
-return true;         
+
+# Make the call to the client.
+$result = $mgClient->sendMessage($domain,
+  array('from'    => $email_address,
+        'to'      => $to,
+        'subject' => "Formulário de contato do site:  $name",
+        'text'    => "Você recebeu uma nova mensagem do formulário de contato do seu site.\n\n"."Veja os detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nTelefonee: $phone\n\nMensagem:\n$message"));       
 ?>
